ee5866c1a5c4e064eca71af985a44ae489f2dc74
l4uy ♥ <isavyxd@outlook.com>
2025-07-08 15:47:33 -0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    intents: [
+        'Guilds', 
+        'GuildMessages', 
+        'MessageContent', 
+        'GuildMembers', 
+        'GuildModeration'
+    ],
+    activity: {
+        name: 'with yarn balls',
+        type: 'PLAYING'
+    }
+};
